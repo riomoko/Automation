759dfc05967f90b85ac654061f99b613c363613d
celestino gianotti <cele@blisscomedia.com>
2025-04-24 08:03:39 +0100

    post now function

--- autoGui.py
+++ autoGui.py
@@ -18,7 +18,7 @@ class AguiTools:
 
     def imageClick(self, image):
         location = agui.locateCenterOnScreen(image, confidence=0.75)
-        agui.moveTo(location, duration=0.4)
+        agui.moveTo(location, duration=0.2)
         agui.click()
 
     def dragAndDrop(self, imageToDrag, imageToDropOn, delay=1.9):
--- mainAuto.py
+++ mainAuto.py
@@ -54,35 +54,27 @@ def imageCheck(image, seconds=20):
         TTS.read("Programma terminato")
         sys.exit()
 
-uploadPlus= "images/uploadBuzz.png"
-videoUploadArea = "images/uploadVideoBuzz.png"
-iconAM="images/am.png"
-hashAstromostro = "images/HashAstromostroBuzz.PNG"
-uploaded="images/uploadedBuzz.PNG"
-post="images/postBuzz.PNG"
-views="images/viewsBuzz.PNG"
-successivo="images/successivoBuzz.PNG"
+def loadVideo():
 
-for i in range(12):
     TTS.read("automazione iniziata")
 
-    agui_tools = AguiTools()
+
     imageCheck(uploadPlus)
     agui_tools.imageClick(uploadPlus)
     time.sleep(random.uniform(0.1, 0.12))
-   # agui_tools.imageClick(uploadPlus)
+    # agui_tools.imageClick(uploadPlus)
     imageCheck(videoUploadArea)
     imageCheck(iconAM)
-    time.sleep(random.uniform(0.1,0.2))
-    dragAndDrop(iconAM,videoUploadArea )
+    time.sleep(random.uniform(0.1, 0.2))
+    dragAndDrop(iconAM, videoUploadArea)
     time.sleep(random.uniform(0.1, 2.1))
     imageCheck(hashAstromostro)
     TTS.read("tag")
     agui_tools.moveToImageCenter(hashAstromostro, random.uniform(0.3, 1.9))
     for i in range(15):
         agui.click()
-      #  TTS.read("t")
-        time.sleep(random.uniform(0.1,0.2))
+        #  TTS.read("t")
+        time.sleep(random.uniform(0.1, 0.2))
     TTS.read("tag finiti")
     time.sleep(random.uniform(0.1, 2.1))
     imageCheck(uploaded, seconds=40)
@@ -90,6 +82,13 @@ for i in range(12):
     time.sleep(random.uniform(0.1, 1.1))
     agui.scroll(-1300)
     TTS.read("scrollato")
+
+
+
+
+def publishNow():
+    loadVideo()
+
     imageCheck(post)
     time.sleep(random.uniform(0.1, 1.1))
     agui_tools.moveToImageCenter(post, random.uniform(0.3, 1.9))
@@ -105,7 +104,40 @@ for i in range(12):
     time.sleep(random.uniform(2, 3.1))
 
 
-time.sleep(random.uniform(2, 3.1))
+def publishBeforeMidnight():
+    loadVideo()
+
+    imageCheck(post)
+    time.sleep(random.uniform(0.1, 1.1))
+    agui_tools.moveToImageCenter(post, random.uniform(0.3, 1.9))
+    time.sleep(random.uniform(0.1, 0.3))
+    TTS.read("posto")
+    agui.click()
+
+    time.sleep(random.uniform(1, 2.1))
+    TTS.read("emozione!!! ")
+    imageCheck(views)
+    imageCheck(successivo)
+    agui_tools.imageClick(successivo)
+    time.sleep(random.uniform(2, 3.1))
+
+
+
+uploadPlus = "images/uploadBuzz.png"
+videoUploadArea = "images/uploadVideoBuzz.png"
+iconAM = "images/am.png"
+hashAstromostro = "images/HashAstromostroBuzz.PNG"
+uploaded = "images/uploadedBuzz.PNG"
+post = "images/postBuzz.PNG"
+views = "images/viewsBuzz.PNG"
+successivo = "images/successivoBuzz.PNG"
+schedule = "images/successivoBuzz.PNG"
+
+agui_tools = AguiTools()
+
+for i in range(12):
+    publishNow()
+
 TTS.read("Terminato")
 sys.exit(0)
 
